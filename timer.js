ce6a3f6a7d94bc7dc81c3d883cf11d006a06bfe2
Giridharmathavaraj2 <152069160+Giridharmathavaraj2@users.noreply.github.com>
2023-12-06 12:44:46 +0530

    Update timer.js
    remove the command in js folder

--- timer.js
+++ timer.js
@@ -3,24 +3,6 @@ const hours =document.querySelector("#hours")
 const minutes =document.querySelector("#minutes")
 const seconds =document.querySelector("#seconds")
 
-
-
-// console.log(new Date());
-// const currentYear = new Date().getFullYear()
-// console.log(currentYear+1)
-// const newYear = new Date(`January 1 ${currentYear+1} 00:00:00`);
-// console.log(newYear);
-// const currentDate =new Date();
-// console.log(currentDate);
-// const diff = newYear-currentDate
-// const  d = Math.floor(diff/1000/60/60/24);
-// console.log(d);
-// const h =Math.floor((diff/1000/60/60)%24);
-// console.log(h);
-// const m =Math.floor((diff/1000/60)%60);
-// console.log(m);
-// const s =Math.floor((diff/1000)%60);
-// console.log(s);
 /*
 1000ms = 1s
 60s = 1min
