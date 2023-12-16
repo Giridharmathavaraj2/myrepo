8ab712e51ba9e80dd93318cc1133242c03959716
Giridharmathavaraj2 <152069160+Giridharmathavaraj2@users.noreply.github.com>
2023-12-16 17:58:47 +0530

    Update new.js
    it was upload twice by giri

--- carousel/new.js
+++ carousel/new.js
@@ -1,42 +1,42 @@
-var sliderPosition = 0;
-var slider = document.querySelectorAll(".slider-item");
-var totalSlider = slider.length;
-var btnPrev = document.querySelector("#btn-prev");
-var btnNext = document.querySelector("#btn-next");
+// var sliderPosition = 0;
+// var slider = document.querySelectorAll(".slider-item");
+// var totalSlider = slider.length;
+// var btnPrev = document.querySelector("#btn-prev");
+// var btnNext = document.querySelector("#btn-next");
 
-console.log(totalSlider);
+// console.log(totalSlider);
 
-function updatePosition() {
-    slider.forEach(slide => {
-        slide.classList.remove('active');
-        slide.classList.add('hidden');
-    });
-    slider[sliderPosition].classList.add('active');
-}
+// function updatePosition() {
+//     slider.forEach(slide => {
+//         slide.classList.remove('active');
+//         slide.classList.add('hidden');
+//     });
+//     slider[sliderPosition].classList.add('active');
+// }
 
-btnNext.addEventListener('click', function () {
-    NextSlide();
-});
+// btnNext.addEventListener('click', function () {
+//     NextSlide();
+// });
 
-btnPrev.addEventListener('click', function () {
-    PrevSlide();
-});
+// btnPrev.addEventListener('click', function () {
+//     PrevSlide();
+// });
 
-function NextSlide() {
-    if (sliderPosition === totalSlider - 1) {
-        sliderPosition = 0;
-    } else {
-        sliderPosition++;
-    }
-    updatePosition();
-}
+// function NextSlide() {
+//     if (sliderPosition === totalSlider - 1) {
+//         sliderPosition = 0;
+//     } else {
+//         sliderPosition++;
+//     }
+//     updatePosition();
+// }
 
-function PrevSlide() {
-    if (sliderPosition === 0) {
-        sliderPosition = totalSlider - 1;
-    } else {
-        sliderPosition--;
-    }
-    updatePosition();
-}
- setInterval(NextSlide,5000)
+// function PrevSlide() {
+//     if (sliderPosition === 0) {
+//         sliderPosition = totalSlider - 1;
+//     } else {
+//         sliderPosition--;
+//     }
+//     updatePosition();
+// }
+//  setInterval(NextSlide,5000)
