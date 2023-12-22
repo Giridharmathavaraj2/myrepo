f662df457526fa60da554c3f648dc6250546075b
Giridharmathavaraj <giridharmathavaraj@gmail.com>
2023-12-22 10:45:13 +0530

    It is like photo gallary by giri

---
+++ photo gallary/photo gallary.js
@@ -0,0 +1,25 @@
+
+
+const model = document.createElement('div');
+model.id = 'model';
+document.body.appendChild(model);
+
+const images = document.querySelectorAll('.img');
+
+images.forEach(image => {
+    image.addEventListener('click', () => {
+        model.classList.add('active');
+        const img = document.createElement('img');
+        img.src = image.src;
+        img.id = 'img';
+        model.appendChild(img);
+        console.log(img.src);
+        while (model.childElementCount > 1) {
+            model.removeChild(model.firstChild);
+        }
+    });
+});
+
+model.addEventListener('click', () => {
+    model.classList.remove('active');
+});
